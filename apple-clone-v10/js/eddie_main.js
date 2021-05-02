48a6ccd88e95d803739265810dcd72b0d654aeb8
Eddie <40482274+eddie0329@users.noreply.github.com>
2021-05-02 21:56:06 +0900

    :sparkles: Implement scroll animation

--- apple-clone-v10/js/eddie_main.js
+++ apple-clone-v10/js/eddie_main.js
@@ -19,7 +19,16 @@
         messageD: document.querySelector("#scroll-section0 .main-message.d"),
       },
       values: {
-        messageA_opacity: [0, 1],
+        // message A
+        messageA_opacity_in: [0, 1, { start: 0.1, end: 0.2 }],
+        messageA_opacity_out: [1, 0, { start: 0.25, end: 0.3 }],
+        messageA_translateY_in: [20, 0, { start: 0.1, end: 0.2 }],
+        messageA_translateY_out: [0, -20, { start: 0.25, end: 0.3 }],
+        // message B
+        messageB_opacity_in: [1, 0, { start: 0.3, end: 0.4 }],
+        // message C
+
+        // message D
       },
     },
     {
@@ -54,10 +63,12 @@
   // 각 스크롤 섹션의 높이 세팅
   function setLayout() {
     for (let i = 0; i < sceneInfo.length; i++) {
-      sceneInfo[i].scrollHeight = sceneInfo[i].heightNum * window.innerHeight;
-      sceneInfo[
-        i
-      ].objs.container.style.height = `${sceneInfo[i].scrollHeight}px`;
+      if (sceneInfo[i].type === "sticky") {
+        sceneInfo[i].scrollHeight = sceneInfo[i].heightNum * window.innerHeight;
+      } else if (sceneInfo[i].type === "normal") {
+        sceneInfo[i].scrollHeight = sceneInfo[i].objs.container.offsetHeight;
+      }
+      sceneInfo[i].objs.container.style.height = `${sceneInfo[i].scrollHeight}px`;
     }
 
     yOffset = window.pageYOffset;
@@ -74,12 +85,32 @@
   }
 
   function calcValues(values, currentYOffset) {
-    const [ messageA_opacity_0, messageA_opacity_1 ] = values.messageA_opacity;
     let rv;
-    let scrollRatio = currentYOffset / sceneInfo[currentScene].scrollHeight;
-    
-    rv = scrollRatio * (messageA_opacity_1 - messageA_opacity_0) + messageA_opacity_0;
+    const scrollHeight = sceneInfo[currentScene].scrollHeight;
+    const scrollRatio = currentYOffset / scrollHeight;
+    if (values.length === 3) {
+      // start ~ end  사이에 에니메이션 실행
+      const partScrollStart = values[2].start * scrollHeight;
+      const partScrollEnd = values[2].end * scrollHeight;
+      const partScrollHeight = partScrollEnd - partScrollStart;
 
+      if (
+        currentYOffset >= partScrollStart &&
+        currentYOffset <= partScrollEnd
+      ) {
+        rv =
+          ((currentYOffset - partScrollStart) / partScrollHeight) *
+            (values[1] - values[0]) +
+          values[0];
+      } else if (currentYOffset < partScrollStart) {
+        rv = values[0];
+      } else if (currentYOffset > partScrollEnd) {
+        rv = values[1];
+      }
+    } else {
+      rv = scrollRatio * (values[1] - values[0]) + values[0];
+    }
+    console.log(rv);
     return rv;
   }
 
@@ -87,12 +118,37 @@
     const { objs } = sceneInfo[currentScene];
     const { values } = sceneInfo[currentScene];
     const currentYOffset = yOffset - prevScrollHeight;
+    const scrollHeight = sceneInfo[currentScene].scrollHeight;
+    const scrollRatio = currentYOffset / scrollHeight;
 
     switch (currentScene) {
       case 0:
-        const messageA_opacity_in = calcValues(values, currentYOffset);
-        objs.messageA.style.opacity = messageA_opacity_in;
-        console.log(messageA_opacity_in);
+        const messageA_opacity_in = calcValues(
+          values.messageA_opacity_in,
+          currentYOffset
+        );
+        const messageA_opacity_out = calcValues(
+          values.messageA_opacity_out,
+          currentYOffset
+        );
+        const messageA_translateY_in = calcValues(
+          values.messageA_translateY_in,
+          currentYOffset
+        );
+        const messageA_translateY_out = calcValues(
+          values.messageA_translateY_out,
+          currentYOffset
+        );
+        if (scrollRatio <= 0.22) {
+          // in
+          objs.messageA.style.opacity = messageA_opacity_in;
+          objs.messageA.style.transform = `translateY(${messageA_translateY_in}%)`;
+        } else {
+          // out
+          objs.messageA.style.opacity = messageA_opacity_out;
+          objs.messageA.style.transform = `translateY(${messageA_translateY_out}%)`;
+        }
+
         break;
       case 1:
         break;
